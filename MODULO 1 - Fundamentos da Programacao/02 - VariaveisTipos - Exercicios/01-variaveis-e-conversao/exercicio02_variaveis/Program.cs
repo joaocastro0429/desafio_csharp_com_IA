4f86806cd161696e7da147e465e489f532e29c2f
joaocastro0429 <joaocastro0429@gmail.com>
2026-03-02 06:03:18 -0300

    exercicio de variaveis

---
+++ MODULO 1 - Fundamentos da Programacao/02 - VariaveisTipos - Exercicios/01-variaveis-e-conversao/exercicio02_variaveis/Program.cs
@@ -0,0 +1,14 @@
+﻿string nome = "João";
+int idade = 20;
+
+// dinâmica
+var nome2 = "Marcos";
+var idade2 = 20; // agora sim existe idade2
+
+var resultado1 = Convert.ToString(idade2) + " - " + nome2;
+var resultado2 = idade2.ToString() + " - " + nome2;
+var resultado3 = int.Parse("2") + idade2;
+
+Console.WriteLine(resultado1);
+Console.WriteLine(resultado2);
+Console.WriteLine(resultado3);
